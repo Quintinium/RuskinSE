7b67b8f2aaae4537f064d6953a9931a93fb999d1
elenamizell <elena.mizell@selu.edu>
2016-03-06 12:07:12 -0600

    program to populate database with garth
    basic puesdo code

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//program to populate search database
+//authors: Elena, Garth
+
+//open directory with xml files
+//loop through all files
+  //open file
+  //extract information from document
+  //extract title
+  //extract doctype                                       
+  //extract meter
+  //extract rhyme
+  //extract text
+  //extract url
+  //insert into document table
+  
+    //start loop to grab all tags
+    //extract keywords from document
+    //extract tag
+    //extract subtag
+    //extract content
+    //insert into keyword table with corrs docid
+    //end tag loop
+   
+//end loop        
+                                                                     
+?>
