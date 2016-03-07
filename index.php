c8974ac494114b45173f9692fb620eb0194ddade
GarthKiepper <garth.kiepper@selu.edu>
2016-03-07 10:04:09 -0600

    Added links to relevant PHP documentation.
    I have researched the necessary PHP functions we will need in order to
    develop this program. I have included links to the documentation pages
    for each function.

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
 
 //open directory with xml files
 //loop through all files
+// Example #2 in the PHP documentation shows how to loop through all of the files in a directory: http://php.net/manual/en/function.readdir.php
+
   //open file
+  // Examples #1 and #2 explain how to read the contents of a file and print them to the screen: http://php.net/manual/en/function.file-get-contents.php
+  
   //extract information from document
+  // We will need the following PHP functions to extract information from our XML documents:
+  // substr: http://php.net/manual/en/function.substr.php
+  // strlen: http://php.net/manual/en/function.strlen.php
+  // strpos: http://php.net/manual/en/function.strpos.php
+  // simplexml_load_file: http://php.net/manual/en/function.simplexml-load-file.php
+  // json_encode: http://php.net/manual/en/function.json-encode.php
+  // json_decode: http://php.net/manual/en/function.json-decode.php
+  
+  
   //extract title
   //extract doctype                                       
   //extract meter
   //extract rhyme
   //extract text
   //extract url
+  
   //insert into document table
+  // MySQL insert command: http://www.w3schools.com/php/php_mysql_insert.asp
   
     //start loop to grab all tags
     //extract keywords from document
@@ -22,6 +37,5 @@
     //insert into keyword table with corrs docid
     //end tag loop
    
-//end loop        
-                                                                     
+//end loop
 ?>
