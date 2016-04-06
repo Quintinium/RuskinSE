8e78128dd3e07e097bf7b9b71a58fbaab1f492f1
jay3417 <moore1309@outlook.com>
2016-04-06 16:52:02 -0500

    Started testing some backend coding with Garth

---
+++ Backend.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ruskin Search</title>
+	</head>
+	<body>
+		<form method="POST">
+			Keyword(s): <input type="text" name="keyword"><br /><br />
+			<input type="checkbox" name="is_poem_document" value="true"> Is a poem?<br />
+			<input type="checkbox" name="activate_document_filter" value="true">Show documents that are:<select name="divtype_document"> 
+				<option value="apparatus">apparatus</option>
+				<option value="poem">poem</option>
+				<option value="note">note</option>
+			</select><br />
+			<input type="checkbox" name="full_text_of_document" value="true"> Search full text<br />
+			<input type="checkbox" name="activate_tag_filter" value="true">Show results if keyword is a:
+			<select name="tag_keywords">				
+				<option value="persName">Person Name</option>
+				<option value="geogName">Geographical Name</option>
+				<option value="placeName">Place Name</option>
+				<option value="orgName">Organization Name</option>
+				<option value="title">Title</option>
+				<option value="name">Name</option>
+			</select>
+			of a: 
+			<select name="type_keywords">
+				<option value="aa">Anything/Anyone</option>
+				<option value="book">Book</option>
+				<option value="poem">Poem</option>
+				<option value="pen_name">Pen Name</option>
+				<option value="periodical">Periodical</option>
+				<option value="essay">Essay</option>
+				<option value="bibliography">Bibliography</option>
+				<option value="prose">Prose</option>
+				<option value="archive">Archive</option>
+				<option value="engraving">Engraving</option>
+				<option value="collected_poems">Collected Poems</option>
+				<option value="collected_sketches">Collected Sketches</option>
+				<option value="anthology">Anthology</option>
+				<option value="reference">Reference</option>
+				<option value="biography">Biography</option>
+				<option value="novel">Novel</option>
+				<option value="magazine">Magazine</option>
+				<option value="drama">Drama</option>
+				<option value="collected_letters">Collected Letters</option>
+				<option value="autobiography">Autobiography</option>
+				<option value="series">Series</option>
+				<option value="peom">Peom</option>
+				<option value="guidebook">Guidebook</option>
+				<option value="manuscript">Manuscript</option>
+				<option value="lesson">Lesson</option>
+			</select><br />
+			<input type="submit" name="submit" value=" Search ">
+		</form>
+<?php
+
+// MySQL server credentials.
+$servername = 'localhost';
+$username = 'root';
+$password = 'zxcvbnm';
+$database = 'ruskin';
+
+mysql_connect($servername, $username, $password);
+mysql_select_db($database);
+
+if (isset($_POST['keyword'])) {
+	echo '<br />';
+	echo "Keyword: " . $_POST['keyword'];
+	echo '<br />';
+	
+	echo 'Poem: ';
+	
+	if (isset($_POST['is_poem_document'])) {
+		echo 'Exist';
+	} else {
+		echo 'Failed';
+	}
+	
+	echo'<br />Document Filter: ';
+	
+	if (isset($_POST['activate_document_filter'])) {
+		echo 'Exist';
+	} else {
+		echo 'Failed';
+	}	
+	
+	echo '<br />Divtype Document: ' . $_POST['divtype_document'];
+	
+	echo '<br />Full Text: ';
+	
+	if (isset($_POST['full_text_of_document'])) {
+		echo 'Exist';
+	} else {
+		echo 'Failed';
+	}
+			
+	$results = mysql_query("SELECT * FROM `documents` WHERE `text` LIKE '%" . mysql_real_escape_string($_POST['keyword']) . "%' ORDER BY `documents`.`title` DESC");
+	
+	echo '<div class="container results-container">
+	<h2>Search results for <span class="italic">"Ruskin"</span> :</h2>
+			<div class="divider"></div>';
+			
+	while ($row = mysql_fetch_assoc($results)) {
+		echo '<div class="result-container">
+				<b>Location:</b> <a class="italic" href="' . $row['url'] . '">' . $row['url'] . '</a><br />
+				<b>Matching text:</b> <span class="italic">' . $_POST['keyword'] . '</span><br />
+				<b>Document type:</b> ' . $row['divtype'] . '
+			</div>';
+	}
+	echo '</div>';
+}
+
+?>
+	</body>
+</html>
