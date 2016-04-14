8b133f76c8e4dbabec367c2cd2d3991cbc57229f
Kevin Dyer <kevin.dyer@selu.edu>
2016-04-14 09:05:11 -0500

    Made results page look like website - with Elena
    changed the Backend.php to display the nav bar on the side

--- Backend.php
+++ Backend.php
@@ -1,96 +1,53 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Ruskin Search</title>
-	</head>
-	<body>
-		<form method="POST">
-			Keyword(s): <input type="text" name="keyword"><br /><br />
-			<input type="checkbox" name="is_poem_document" value="true"> Is a poem?<br />
-			<input type="checkbox" name="activate_document_filter" value="true">Show documents that are:
-			<select name="divtype_document"> 
-				<option value="apparatus">Apparatus</option>
-				<option value="poem">Poem</option>
-				<option value="note">Note</option>
-			</select><br />
-			<input type="checkbox" name="full_text_of_document" value="true"> Search full text<br />
-			<input type="checkbox" name="activate_tag_filter" value="true">Show results if keyword is a:
-			<select name="tag_keywords">				
-				<option value="persName">Person Name</option>
-				<option value="geogName">Geographical Name</option>
-				<option value="placeName">Place Name</option>
-				<option value="orgName">Organization Name</option>
-				<option value="title">Title</option>
-				<option value="name">Name</option>
-				<option value="date">Date</option>
-			</select>
-			of a: 
-			<select name="type_keywords">
-				<option value="aa">Anything/Anyone</option>
-				<option value="book">Book</option>
-				<option value="poem">Poem</option>
-				<option value="pen_name">Pen Name</option>
-				<option value="periodical">Periodical</option>
-				<option value="essay">Essay</option>
-				<option value="bibliography">Bibliography</option>
-				<option value="prose">Prose</option>
-				<option value="archive">Archive</option>
-				<option value="engraving">Engraving</option>
-				<option value="collected_poems">Collected Poems</option>
-				<option value="collected_sketches">Collected Sketches</option>
-				<option value="anthology">Anthology</option>
-				<option value="reference">Reference</option>
-				<option value="biography">Biography</option>
-				<option value="novel">Novel</option>
-				<option value="magazine">Magazine</option>
-				<option value="drama">Drama</option>
-				<option value="collected_letters">Collected Letters</option>
-				<option value="autobiography">Autobiography</option>
-				<option value="bibliography_text">Bibliography Text</option>
-				<option value="manuscript">Manuscript</option>
-				<option value="program">Program</option>
-				<option value="lesson">Lesson</option>
-				<option value="gloss">Gloss</option>
-				<option value="composite">Composite</option>
-				<option value="sermon">Sermon</option>
-				<option value="archive_digital">Archive Digital</option>
-				<option value="archive_nondigital">Archive Non-Digital</option>
-				<option value="series">Series</option>
-				<option value="fictional">Fictional</option>
-				<option value="story">Story</option>
-				<option value="peom">Peom</option>
-				<option value="architecture">Architecture</option>
-				<option value="collected_works">Collected Works</option>
-				<option value="painting">Painting</option>
-				<option value="letter">Letter</option>
-				<option value="guidebook">Guidebook</option>
-				<option value="constellation">Constellation</option>
-				<option value="book_chapter">Book Chapter</option>
-				<option value="memoir">Memoir</option>
-				<option value="composition">Composition</option>
-				<option value="work">Work</option>
-				<option value="scripture">Scripture</option>
-				<option value="drawing">Drawing</option>
-				<option value="collected_art">Collected Art</option>
-				<option value="sketch">Sketch</option>
-				<option value="map">Map</option>
-				<option value="catalog_auction">Catalog Auction</option>
-				<option value="dictionary">Dictionary</option>
-				<option value="ana">Ana</option>
-				<option value="collecte_poems">Collected Poems</option>
-				<option value="tale">Tale</option>
-				<option value="other">Other</option>
-				<option value="article">Article</option>
-			</select><br />
-			<input type="submit" name="submit" value=" Search ">
-		</form>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0048)http://english.selu.edu/humanitiesonline/ruskin/ -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<title>The Early Ruskin Manuscripts, Advanced Search Results</title>
+	<link href="./The Early Ruskin Manuscripts, 1826–1842_files/styles.css" rel="stylesheet" type="text/css">
+	<link rel="icon" type="image/png" href="http://english.selu.edu/humanitiesonline/ruskin/images/ruskin_icon.png">
+</head>
+<body>
+		<div class="navigation">
+        <a href="http://english.selu.edu/humanitiesonline/ruskin/index.php"><img src="./The Early Ruskin Manuscripts, 1826–1842_files/ruskin_logo.jpg" width="100%"></a>
+        <br>
+		<br>
+        <span class="navigation-title">
+            The
+            <br>Early
+            <br>Ruskin
+            <br>Manuscripts
+            <div class="navigation-date">1826–1842</div>
+        </span>
+        <hr>
+        <span class="navigation-editor"><a href="mailto:ruskinproject@selu.edu">David C. Hanson, Editor</a></span>
+        <br>
+        <br>
+        <span><a href="http://english.selu.edu/humanitiesonline/ruskin/index.php">Home</a><span>
+        <span>
+        	<br><a href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php">Indices</a>
+            <br><a class="subnavigation" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#WORKS">Works</a>
+            <br><a class="subnavigation" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#MANUSCRIPTS">Manuscripts</a>
+            <br><a class="subnavigation" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#CORPORA">Corpora</a>
+            <br><a class="subnavigation" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#WORKSBYOTHERS">Works by Others</a>
+            <br><a class="subnavigation" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#COMMENTARY">Commentary</a>
+            <br><a class="tion" href="http://english.selu.edu/humanitiesonline/ruskin/essays/indices_essay.php#ESSAYS">Essays</a>
+        </span>
+        <br>
+		<span>XML</span>        <br>
+		<span>Bibliography</span>
+        <br>
+        <span><a href="http://english.selu.edu/humanitiesonline/ruskin/webpages/staff.php">Staff and Support</a><span>
+        <br>
+        <span><a href="http://english.selu.edu/humanitiesonline/ruskin/webpages/legal.php">Legal</a><span>
+		 <br>
+        <span><a href="http://localhost/search.php">Search</a><span>
+	</span></span></span></span></span></span></span></span></div>	
 <?php
 
 // MySQL server credentials.
 $servername = 'localhost';
 $username = 'root';
-$password = 'zxcvbnm';
+$password = 'kev#dr16';
 $database = 'ruskin';
 
 mysql_connect($servername, $username, $password);
